16c3518b10503430fb25a96f8b7fb2a6c49ed548
jebeniani <aymenjebeniani05@gmail.com>
2023-01-04 13:27:06 +0100

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a method that calculates the fewest number of operations needed to result in exactly "n" "H" charachters in the file.
+"""
+import math
+
+
+def minOperations(n):
+    if n < 2:
+        return 0
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(n // i) + i
