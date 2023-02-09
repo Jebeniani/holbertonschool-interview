db2345fd5e098a4fccbfb146b79e4d445ce7d095
jebeniani <aymenjebeniani@gmail.com>
2023-02-09 22:42:56 +0100

    why always me

---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,18 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: function that checks whether or not
+ * a given unsigned integer is a palindrome
+ */
+
+int is_palindrome(unsigned long n);
+
+#endif /* PALINDROME_H */
