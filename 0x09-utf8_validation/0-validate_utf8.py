2915cf7e9e7c6e91686c9c50d74e9989d74620a5
jebeniani <aymenjebeniani@gmail.com>
2023-02-16 23:10:41 +0100

    UTF-8 Validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,27 +3,21 @@
 
 
 def validUTF8(data):
-    """ethod that determines if a given data set represents a valid UTF-8"""
+    """method that determines if a given data set represents a valid UTF-8"""
     num_bytes = 0
-
     mask1 = 1 << 7
     mask2 = 1 << 6
-
     for byte in data:
         if num_bytes == 0:
             while mask1 & byte:
                 num_bytes += 1
                 mask1 = mask1 >> 1
-
             if num_bytes == 0:
                 continue
-
             if num_bytes > 4 or num_bytes == 1:
                 return False
         else:
             if not (byte & mask1 and not (byte & mask2)):
                 return False
-
         num_bytes -= 1
-
     return num_bytes == 0
