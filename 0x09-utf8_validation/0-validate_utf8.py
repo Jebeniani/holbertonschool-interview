3095ad38555f2816f4756fed366ff3812f086edd
jebeniani <aymenjebeniani@gmail.com>
2023-02-16 23:14:40 +0100

    UTF-8 Validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,11 +7,13 @@ def validUTF8(data):
     num_bytes = 0
     mask1 = 1 << 7
     mask2 = 1 << 6
+
     for byte in data:
         if num_bytes == 0:
             while mask1 & byte:
                 num_bytes += 1
-                mask1 = mask1 >> 1
+                mask1 >>= 1
+
             if num_bytes == 0:
                 continue
             if num_bytes > 4 or num_bytes == 1:
