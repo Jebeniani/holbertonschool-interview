7e358a04a097e96ad7a882205746ba030e83f7b9
jebeniani <aymenjebeniani@gmail.com>
2023-02-23 09:44:54 +0100

    little update

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -4,7 +4,7 @@
  * slide_line - slides and merges an array of integers to the left or to the right
  * @line: a pointer to the array of integers
  * @size: the size of the array
- * @direction: the direction to slide and merge the array
+ * @direction: direction to slide and merge the arraygit
  *
  *
  * Return: 1 upon success, or 0 upon failure
@@ -12,9 +12,10 @@
 int slide_line(int *line, size_t size, int direction)
 {
     size_t i, j, step;
+    int merged[LINE_SIZE] = {0};
 
     if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
-        return (0);
+        return 0;
 
     if (direction == SLIDE_LEFT)
         step = 1;
@@ -23,38 +24,32 @@ int slide_line(int *line, size_t size, int direction)
 
     for (i = 0; i < size; i++)
     {
-        if (line[i] != 0)
+        if (line[i] == 0)
+            continue;
+
+        for (j = i + step; j < size; j += step)
         {
-            for (j = i + step; j < size && (int)j >= 0; j += step)
+            if (line[j] == 0)
+                continue;
+
+            if (line[i] == line[j] && merged[j] == 0 && i != j)
             {
-                if (line[j] != 0)
-                {
-                    if (line[i] == line[j])
-                    {
-                        line[i] *= 2;
-                        line[j] = 0;
-                        break;
-                    }
-                    break;
-                }
+                line[i] *= 2;
+                line[j] = 0;
+                merged[i] = 1;
+                break;
             }
+
+            if (line[i] != line[j] && line[j - step] == 0)
+            {
+                line[j - step] = line[j];
+                line[j] = 0;
+                j -= step;
+            }
+            else
+                break;
         }
     }
 
-    for (i = 0, j = 0; i < size; i++)
-    {
-        if (line[i] != 0)
-        {
-            line[j] = line[i];
-            j++;
-        }
-    }
-
-    while (j < size)
-    {
-        line[j] = 0;
-        j++;
-    }
-
-    return (1);
+    return 1;
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -4,6 +4,8 @@
 
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 2
+#define LINE_SIZE 32
+
 
 int slide_line(int *line, size_t size, int direction);
 
