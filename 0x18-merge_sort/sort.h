6a8c82a0e316c59ab1564fa7edf1068c3198be72
jebeniani <aymenjebeniani05@gmail.com>
2023-06-22 23:06:11 +0100

    Header file

--- 0x18-merge_sort/sort.h
+++ 0x18-merge_sort/sort.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 void print_array(const int *array, size_t size);
+void merge(int *array, size_t size, int *tmp);
 void merge_sort(int *array, size_t size);
 
 #endif /* SORT_H */
