ce355b188709d59f6fca2c0e7ba7f76d47ee7f79
jebeniani <aymenjebeniani05@gmail.com>
2023-07-06 11:39:25 +0100

    Change comes from within

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+
+    return dp[total]
