6ad4fc65a3010b71088b9d99673a9731274b1236
jebeniani <aymenjebeniani05@gmail.com>
2023-10-06 14:51:50 +0100

    2D Menger sponge

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,53 +1,43 @@
-#include <stdio.h>
-#include <math.h>
 #include "menger.h"
-
 /**
  * menger - Draws a square of a given size using the '#' character.
- * @level: The level
- * return: The size of the square
+ *
+ * @level: level of menger sponge
+ *
+ * Return: void
  */
 void menger(int level)
 {
+int i_size = pow(3, level);
+
+int i_row = 0, i_rhigh = 0, i_colum = 0, i_chigh = 0;
+char c_print = '#';
+
 if (level < 0)
 {
 return;
 }
-int size = pow(3, level);
-draw_sponge(level, 0, 0, size);
+
+for (; i_colum < i_size; i_colum++)
+{
+for (i_row = 0; i_row < i_size; i_row++)
+{
+c_print = '#';
+i_rhigh = i_row;
+i_chigh = i_colum;
+while (i_rhigh > 0 && i_chigh > 0)
+{
+if (i_rhigh % 3 == 1 && i_chigh % 3 == 1)
+{
+c_print = ' ';
+}
+i_rhigh /= 3;
+i_chigh /= 3;
 }
 
-/**
- * draw_sponge - Draws a 2D Menger Sponge of
- * a given level using the '#' and ' ' characters.
- * @level: The level to draw
- * @x: The x position
- * @y: The y position
- * @size: The size of the square
- *
- * return: The
- */
-void draw_sponge(int level, int x, int y, int size)
-{
-if (level == 0)
-{
-printf("#");
-return;
+printf("%c", c_print);
 }
-int sub_size = size / 3;
-for (int i = 0; i < 3; i++)
-{
-for (int j = 0; j < 3; j++)
-{
-if (i == 1 && j == 1)
-{
-printf(" ");
-}
-else
-{
-draw_sponge(level - 1, x + i * sub_size, y + j * sub_size, sub_size);
-}
-}
+
 printf("\n");
 }
 }
