a1ba485fe6b9d0e5ce0d17c12d7d754bca184ee6
jebeniani <aymenjebeniani05@gmail.com>
2023-10-11 12:32:02 +0100

    header file

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+void print_array(const int *array, size_t size);
+void swap_int(int *a, int *b);
+void heapify(int *array, size_t size, size_t i, size_t tot_sz);
+void heap_sort(int *array, size_t size);
+
+#endif
