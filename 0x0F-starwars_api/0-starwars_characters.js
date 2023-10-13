480ddc8dbe497f2e76439ef73101cb2ed7fcffe1
jebeniani <aymenjebeniani05@gmail.com>
2023-10-13 13:29:18 +0100

    Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,28 +1,37 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie.
 
+const args = process.argv.slice(2);
+if (args < 1) {
+  console.log('Error');
+  process.exit(1);
+}
+
+const episode = args[0];
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(url, async (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const characterUrl of characters) {
-      await new Promise((resolve, reject) => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error(error);
-          } else {
-            const character = JSON.parse(body);
-            console.log(character.name);
-          }
-          resolve();
-        });
-      });
+let characters = [];
+request(url, (err, resp, body) => {
+  if (err || resp.statusCode !== 200) console.log(err);
+  else characters = JSON.parse(body).characters;
+  const size = Object.keys(characters).length;
+  const array = Array(size).fill();
+  let data = 0;
+  for (let i = 0; i < size; i++) {
+    request(characters[i], (erro, respo, bodys) => {
+      if (erro || respo.statusCode !== 200) console.log(erro);
+      else {
+        array[i] = JSON.parse(bodys).name;
+        data++;
+      }
+      if (data === size) {
+        for (let j = 0; j < size; j++) {
+          console.log(array[j]);
+        }
+      }
     }
+
+    );
   }
 });
