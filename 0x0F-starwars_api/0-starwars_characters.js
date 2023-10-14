af660db0121d37bc71ed76ab075677cbeb8fc9be
jebeniani <aymenjebeniani05@gmail.com>
2023-10-14 12:43:25 +0100

    Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-    Script that prints all characters of a Star Wars movie
+    Script that prints all characters of a Star Wars movie.
 */
 
 const args = process.argv.slice(2);
